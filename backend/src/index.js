70fa5eac8aecf552489be890f61c6b5ee815275e
Usupek <ahmadzainurafialfikri@gmail.com>
2025-01-01 17:59:09 +0700

    added message route and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
